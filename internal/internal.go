15c07c754037ddb4d1d0234a89f6db27f98bf968
Will Roden <willabides@github.com>
2020-08-09 16:52:19 -0500

    remove idmaps

--- internal/internal.go
+++ internal/internal.go
@@ -3,23 +3,14 @@ package internal
 import (
 	"context"
 	"fmt"
-	"reflect"
 
 	"github.com/willabides/octo-go/options"
 )
 
-var (
-	reqOperationIDs     map[string]string //nolint:unused // save for later
-	operationAttributes map[string][]EndpointAttribute
-)
+var operationAttributes map[string][]EndpointAttribute
 
 // EndpointAttribute is an attribute for an endpoint
 type EndpointAttribute int
-
-// OperationAttributes returns the EndpointAttributes associated with an operation id
-func OperationAttributes(id string) []EndpointAttribute {
-	return operationAttributes[id]
-}
 
 // OperationHasAttribute returns true if the operation id the given attribute
 func OperationHasAttribute(id string, attr EndpointAttribute) bool {
@@ -30,15 +21,6 @@ func OperationHasAttribute(id string, attr EndpointAttribute) bool {
 		}
 	}
 	return false
-}
-
-// structName returns the name of a struct from its reflect type or a pointer
-//nolint:unused // save for later
-func structName(tp reflect.Type) string {
-	if tp.Kind() == reflect.Ptr {
-		return structName(tp.Elem())
-	}
-	return tp.Name()
 }
 
 // DoRequest performs an http request and returns a Response
